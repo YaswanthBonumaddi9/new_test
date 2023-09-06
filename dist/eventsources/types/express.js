fedcbf17f469b23a6d656da92a1a82e1320b4976
YaswanthBonumaddi9 <yaswanth@godspeed.systems>
2023-09-06 14:35:56 +0530

    first commit

---
+++ dist/eventsources/types/express.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const plugins_express_as_http_1 = require("@godspeedsystems/plugins-express-as-http");
+exports.default = plugins_express_as_http_1.ExpressEventSource;
+//# sourceMappingURL=express.js.map
